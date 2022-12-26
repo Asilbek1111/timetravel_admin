5552c908f18361c3dd7f80313b2d220faf8c4e39
Azimjon1111 <azimjon.nodir@gmail.com>
2022-12-26 17:20:52 +0500

    DONE

--- axios.js
+++ axios.js
@@ -8,8 +8,4 @@ const instance = axios.create({
   },
 });
 
-// instance.defaults.headers.common[
-//   "Authorization"
-// ] = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl9pZCI6IjRmZjM3YzYzLTg4ZjAtNDE5My04MTJkLWU4OTVkZDQ4MzgzNCIsInVzZXJfaWQiOiI2MzU3NzllNTIyYjEwMjE0M2M2ZjNlZTYiLCJpYXQiOjE2NjY5MzY5MzZ9.QxwtjsSzusqE65IHGYDyu2Tb3x16oIiTix4L7mEwFY8`;
-// https://tourbackendproject.herokuapp.com/
 export default instance;
